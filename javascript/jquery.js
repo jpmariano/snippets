febbb1bc85d6f869f8a4fccca4fc4af2c8d8fc34
johnpaulmariano <john.mariano.ctr@usuhs.edu>
2022-04-11 10:41:42 -0400

    Random Notes

---
+++ javascript/jquery.js
@@ -0,0 +1,5 @@
+//Run jquery on browser console - paste this on your console
+var $ = document.createElement('script');
+jqry.src = "https://code.jquery.com/jquery-3.3.1.min.js";
+document.getElementsByTagName('head')[0].appendChild(jqry);
+jQuery.noConflict();
