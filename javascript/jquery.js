bbadeb7a3491c3e5291cd37d302ba3730fb2b7b6
jmariano <john.mariano.ctr@usuhs.edu>
2024-05-10 17:58:03 -0400

    Added Drupal 8 notes

--- javascript/jquery.js
+++ javascript/jquery.js
@@ -3,3 +3,19 @@ var $ = document.createElement('script');
 jqry.src = "https://code.jquery.com/jquery-3.3.1.min.js";
 document.getElementsByTagName('head')[0].appendChild(jqry);
 jQuery.noConflict();
+
+const isReasonAvailablePromise = function (arrayOfReasons) {
+    return new Promise((resolve, reject) => {
+        let isReasonAvailable = false;
+        for( let reasonkey in arrayOfReasons ) { 
+     
+            if(arrayOfReasons[reasonkey].link !== null){
+                isReasonAvailable = true;
+            } 
+        }
+        if (isReasonAvailable) {
+            resolve(true);
+        }else
+            reject(false);
+    });
+}
